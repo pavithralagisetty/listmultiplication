c1d358e405961cc8d4afda731332942840b62bf3
pavithralagisetty <115474342+pavithralagisetty@users.noreply.github.com>
2022-10-10 19:17:57 +0530

    updated the code for better performance

--- listmultiplication.py
+++ listmultiplication.py
@@ -1,9 +1,9 @@
-import time
-list1=[i for i in range(1000000)]
-list2=[i for i in range(1000000)]
+import numpy as np
+ar1=np.array(list1)
+ar2=np.array(list2)
 to=time.time()
-prod_lists=list(map(lambda x, y:x*y, list1, list2))
+prodlist=ar1*ar2
 tp=time.time()
 print(tp-to)
-list_time=tp-to
-print(list_time)
+numpy_time=tp-to
+print("The ratio of time taken is {}".format(list_time/numpy_time))
