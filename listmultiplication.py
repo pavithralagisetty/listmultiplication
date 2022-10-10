b3832081aa905195ac19a6c7a5108b076479413b
lnagaprasad <32927041+lnagaprasad@users.noreply.github.com>
2022-10-10 18:52:54 +0530

    Create listmultiplication.py
    multiplication of huge list. performance to improve which is taking more time

---
+++ listmultiplication.py
@@ -0,0 +1,8 @@
+import time
+list1=[i for i in range(1000000)]
+list2=[i for i in range(1000000)]
+to=time.time()
+prod_lists=list(map(lambda x, y:x*y, list1, list2))
+tp=time.time()
+print(tp-to)
+list_time=tp-to
